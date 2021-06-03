283afd372d7ebb1325541099b4598f7f94637d0f
Robin Janke <robin.janke@gmx.de>
2021-06-03 20:53:02 +0200

    fixed app module

--- apps/telegram-contact-resolver-backend/src/app/app.module.ts
+++ apps/telegram-contact-resolver-backend/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TelegramModule } from './telegram/telegram.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './shared/config/configuration';
